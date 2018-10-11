d4ef8d212b1180ed9c5fa0ac7c3cc2ed8e0a9689
unknown <lavy.vangala@gmail.com>
2018-10-10 21:43:35 -0500

    test

---
+++ src/test/java/com/qa/test/loginTest.java
@@ -0,0 +1,14 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+	
+	@Test
+	public void logintestclass() {
+		
+			System.out.println("I am from Homepage class");
+			
+	}
+
+}
